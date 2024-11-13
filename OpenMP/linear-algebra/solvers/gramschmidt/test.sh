f97ebcb1bc1a7e657357b0b919b3cae7381b6375
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-11-13 23:03:23 +0100

    Testing facility, a static solution

---
+++ OpenMP/linear-algebra/solvers/gramschmidt/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BENCHMARK_RESULTS_FILE=results.txt
+
+# Copy the specified file in place of gramschmidt.c
+function prepare_src() {
+    cp $1 gramschmidt.c
+    echo $1 >> $BENCHMARK_RESULTS_FILE
+}
+
+function compile() {
+    make EXT_CFLAGS="-DPOLYBENCH_TIME" clean all
+}
+
+function run() {
+    for r in $(seq $1); do
+        ./gramschmidt_acc | tee -a $BENCHMARK_RESULTS_FILE
+    done
+}
+
+echo "Testing base version"
+prepare_src gramschmidt-original.c
+compile > /dev/null
+run 5 times
+
+echo "Testing static-optimized version"
+prepare_src gramschmidt-static-opt.c
+compile > /dev/null
+run 5 times
