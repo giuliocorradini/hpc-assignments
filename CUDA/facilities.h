e6f8e2d2236d238a8d7f8a5b461466e4aed24c2b
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-11-29 11:58:10 +0100

    Import from OpenMP version

---
+++ CUDA/facilities.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Include polybench common header. */
+#include <polybench.h>
+
+/* Default data type is double, default size is 512. */
+#include "gramschmidt.h"
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static void facilities_print_array(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(R, NJ, NJ, nj, nj),
+    DATA_TYPE POLYBENCH_2D(Q, NI, NJ, ni, nj))
+{
+    int i, j;
+
+    fprintf(stderr, "A\n");
+
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++)
+            fprintf(stderr, DATA_PRINTF_MODIFIER, A[i][j]);
+        fprintf(stderr, "\n");
+    }
+
+    fprintf(stderr, "Q\n");
+
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++)
+            fprintf(stderr, DATA_PRINTF_MODIFIER, Q[i][j]);
+        fprintf(stderr, "\n");
+    }
+
+    fprintf(stderr, "R\n");
+
+    for (int i = 0; i < nj; i++) {
+        for (int j = 0; j < nj; j++)
+            fprintf(stderr, DATA_PRINTF_MODIFIER, R[i][j]);
+        fprintf(stderr, "\n");
+    }
+}
+
+static void facilities_print_matrix(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NI, NJ, ni, nj))
+{
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++)
+            fprintf(stderr, DATA_PRINTF_MODIFIER, A[i][j]);
+        printf("\n");
+    }
+}
+
+/* Kernel golden standard. Unoptimized */
+static void facilities_kernel_gs(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(R, NJ, NJ, nj, nj),
+    DATA_TYPE POLYBENCH_2D(Q, NI, NJ, ni, nj))
+{
+    int i, j, k;
+
+    DATA_TYPE nrm;
+
+    for (k = 0; k < _PB_NJ; k++) {
+        nrm = 0;
+
+        for (i = 0; i < _PB_NI; i++)
+            nrm += A[i][k] * A[i][k];
+
+        R[k][k] = sqrt(nrm);
+
+        for (i = 0; i < _PB_NI; i++)
+            Q[i][k] = A[i][k] / R[k][k];
+
+        for (j = k + 1; j < _PB_NJ; j++) {
+            R[k][j] = 0;
+
+            for (i = 0; i < _PB_NI; i++)
+                R[k][j] += Q[i][k] * A[i][j];
+
+            for (i = 0; i < _PB_NI; i++)
+                A[i][j] = A[i][j] - Q[i][k] * R[k][j];
+        }
+    }
+}
+
+#include <assert.h>
+
+static void facilities_compare_results(int ni, int nj, DATA_TYPE POLYBENCH_2D(X, NI, NJ, ni, nj), DATA_TYPE POLYBENCH_2D(X_cmp, NI, NJ, ni, nj))
+{
+#ifdef COMPARE_RESULTS
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++) {
+            if (abs(X[i][j] - X_cmp[i][j]) > 10e-2) {
+                fprintf(stderr, "Different value at (%d, %d)\n", i, j);
+                fprintf(stderr, "%.5f %.5f\n", X[i][j], X_cmp[i][j]);
+                assert(0);
+            }
+        }
+    }
+#endif
+}
---
+++ CUDA/transpose.h
@@ -0,0 +1,155 @@
+#pragma once
+#include "gramschmidt.h"
+#include <polybench.h>
+
+/**Funzione per trasformare la matrice in trasposta */
+static void transpose_matrix(int ni, int nj,
+    DATA_TYPE* M, DATA_TYPE* M_T)
+{
+    // Transpose the matrix M, to read its columns into cache as rows
+
+#pragma omp parallel for simd num_threads(NTHREADS) schedule(static) collapse(2)
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++) {
+            M_T[j * nj + i] = M[i * ni + j];
+        }
+    }
+}
+
+#ifndef OPTIMIZATION
+static void kernel_gramschmidt(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(R, NJ, NJ, nj, nj),
+    DATA_TYPE POLYBENCH_2D(Q, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(A_T, NJ, NI, ni, nj),
+    DATA_TYPE POLYBENCH_2D(Q_T, NJ, NI, ni, nj))
+{
+    int i, j, k;
+
+    DATA_TYPE nrm;
+
+    transpose_matrix(ni, nj, A, A_T);
+    transpose_matrix(ni, nj, Q, Q_T);
+
+    for (k = 0; k < _PB_NJ; k++) {
+        nrm = 0;
+
+        for (i = 0; i < _PB_NI; i++)
+            nrm += A_T[k][i] * A_T[k][i];
+
+        R[k][k] = sqrt(nrm);
+
+        for (i = 0; i < _PB_NI; i++)
+            Q_T[k][i] = A_T[k][i] / R[k][k];
+
+        for (j = k + 1; j < _PB_NJ; j++) {
+            R[k][j] = 0;
+
+            for (i = 0; i < _PB_NI; i++)
+                R[k][j] += Q_T[k][i] * A_T[j][i];
+
+            for (i = 0; i < _PB_NI; i++)
+                A_T[j][i] = A_T[j][i] - Q_T[k][i] * R[k][j];
+        }
+    }
+
+    transpose_matrix(ni, nj, Q_T, Q);
+}
+#elif OPTIMIZATION == static
+static void kernel_gramschmidt(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NJ, NI, nj, ni),
+    DATA_TYPE POLYBENCH_2D(R, NJ, NJ, nj, nj),
+    DATA_TYPE POLYBENCH_2D(Q, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(A_T, NJ, NI, ni, nj),
+    DATA_TYPE POLYBENCH_2D(Q_T, NJ, NI, ni, nj))
+{
+    int i, j, k;
+
+    DATA_TYPE nrm;
+
+    transpose_matrix(ni, nj, A, A_T);
+    transpose_matrix(ni, nj, Q, Q_T);
+
+    for (k = 0; k < _PB_NJ; k++) {
+        // Consideriamo la colonna k-esima di A
+        nrm = 0;
+
+        //  Calcoliamo la norma di A^(k)
+        #pragma omp parallel for simd reduction(+ \
+                                        : nrm)
+        for (i = 0; i < _PB_NI; i++)
+            nrm += A_T[k][i] * A_T[k][i];
+
+        //  che viene salvata in nel k-esimo elemento diagonale di R
+        R[k][k] = sqrt(nrm);
+
+        #pragma omp parallel for simd num_threads(NTHREADS) schedule(static)
+        for (i = 0; i < _PB_NI; i++)
+            Q_T[k][i] = A_T[k][i] / R[k][k];
+
+        #pragma omp parallel for schedule(static) num_threads(NTHREADS)
+        for (j = k + 1; j < _PB_NJ; j++) {
+            R[k][j] = 0;
+
+            // R alla riga k, colonna j è il prodotto della k-esima colonna di Q per la j-esima colonna di A
+            //reduction
+            for (i = 0; i < _PB_NI; i++)
+                R[k][j] += Q_T[k][i] * A_T[j][i];
+
+            // aggiorno la colonna i-esima di A con il prodotto element-wise tra colonna k-esima di Q e j-esima di R
+            for (i = 0; i < _PB_NI; i++)
+                A_T[j][i] = A_T[j][i] - Q_T[k][i] * R[k][j];
+        }
+    }
+
+    transpose_matrix(ni, nj, Q_T, Q);
+}
+#elif OPTIMIZATION == workerthreads
+static void kernel_gramschmidt(int ni, int nj,
+    DATA_TYPE POLYBENCH_2D(A, NJ, NI, nj, ni),
+    DATA_TYPE POLYBENCH_2D(R, NJ, NJ, nj, nj),
+    DATA_TYPE POLYBENCH_2D(Q, NI, NJ, ni, nj),
+    DATA_TYPE POLYBENCH_2D(A_T, NJ, NI, ni, nj),
+    DATA_TYPE POLYBENCH_2D(Q_T, NJ, NI, ni, nj))
+{
+    int i, j, k;
+
+    DATA_TYPE nrm;
+
+    transpose_matrix(ni, nj, A, A_T);
+    transpose_matrix(ni, nj, Q, Q_T);
+
+    DATA_TYPE nrm;
+
+    #pragma omp parallel num_threads(NTHREADS) private(i, j, k) //spawn a group of threads
+    {
+        #pragma omp single
+        for (k = 0; k < _PB_NJ; k++) {
+            nrm = 0;
+
+            #pragma omp taskloop shared(nrm)
+            for (i = 0; i < _PB_NI; i++)
+                nrm += A_T[k][i] * A_T[k][i];
+
+            R[k][k] = sqrt(nrm);
+
+            #pragma omp taskloop
+            for (i = 0; i < _PB_NI; i++)
+                Q_T[k][i] = A_T[k][i] / R[k][k];
+
+            #pragma omp taskloop
+            for (j = k + 1; j < _PB_NJ; j++) {
+                R[k][j] = 0;
+
+                for (i = 0; i < _PB_NI; i++)
+                    R[k][j] += Q_T[k][i] * A_T[j][i];
+
+                for (i = 0; i < _PB_NI; i++)
+                    A_T[j][i] = A_T[j][i] - Q_T[k][i] * R[k][j];
+            }
+        }
+    }
+
+    transpose_matrix(ni, nj, Q_T, Q);
+}
+#endif //OPTIMIZATION
